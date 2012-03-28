83bf176d979e4b77bd34445d66e08a0812885b9a
DetroitPro <eric@polerecky.com>
2012-03-27 20:56:02 -0400

    pushing this stuff to github so it's someplcae.

---
+++ util-linux-ng-getopt/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿try {
+  $sysDrive = $env:SystemDrive
+  $gitBinPath = "$sysDrive\Program Files (x86)\Git\bin"
+  
+  $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $installerDir = Join-Path $scriptDir 'installer'
+  
+  Install-ChocolateyZipPackage 'util-linux-ng-bin' 'http://downloads.sourceforge.net/gnuwin32/util-linux-ng-2.14.1-bin.zip' "$installerDir"
+
+  $getoptExe = Join-Path $installerDir 'bin\getopt.exe'
+  
+  Copy-Item "$getoptExe" "$gitBinPath"
+  
+  write-host 'util-linux-ng-bin has been added to git install directory'
+  Write-ChocolateySuccess 'util-linux-ng-bin'
+} catch {
+  Write-ChocolateyFailure 'util-linux-ng-bin' $($_.Exception.Message)
+  throw 
+}
+
+
+
+  
---
+++ util-linux-ng-libintl3/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿try {
+  $sysDrive = $env:SystemDrive
+  $gitBinPath = "$sysDrive\Program Files (x86)\Git\bin"
+  
+  $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $installerDir = Join-Path $scriptDir 'installer'
+  
+  Install-ChocolateyZipPackage 'util-linux-ng-libintl3' 'http://downloads.sourceforge.net/gnuwin32/util-linux-ng-2.14.1-dep.zip' "$installerDir"
+
+  $libintl3Dll = Join-Path $installerDir 'bin\libintl3.dll'
+  
+  Copy-Item "$libintl3Dll" "$gitBinPath"
+  
+  write-host 'libintl3.dll has been added to git install directory'
+  Write-ChocolateySuccess 'util-linux-ng-libintl3'
+} catch {
+  Write-ChocolateyFailure 'util-linux-ng-libintl3' $($_.Exception.Message)
+  throw 
+}
