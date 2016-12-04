f2e4c989c7cd55102963441ea5d61d765d78e736
Stephen Koller <stephenkoller@gmail.com>
2016-12-04 17:54:40 -0500

    Add script for libiconv2.dll

---
+++ util-linux-ng-libiconv2/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿try {
+  $sysDrive = $env:SystemDrive
+
+  if (Test-Path "$sysDrive\Program Files (x86)\Git\bin") {
+      $gitBinPath = "$sysDrive\Program Files (x86)\Git\bin"
+  } elseif (Test-Path "$sysDrive\Program Files\Git\bin") { 
+      $gitBinPath = "$sysDrive\Program Files\Git\bin"
+  } else {
+    write-host 'could not find git install directory'
+    throw
+  }
+
+  $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+  $installerDir = Join-Path $scriptDir 'installer'
+
+  Install-ChocolateyZipPackage 'util-linux-ng-libiconv2' 'http://downloads.sourceforge.net/gnuwin32/util-linux-ng-2.14.1-dep.zip' "$installerDir"
+
+  $libiconv2Dll = Join-Path $installerDir 'bin\libiconv2.dll'
+
+  Copy-Item "$libiconv2Dll" "$gitBinPath"
+
+  write-host 'libiconv2.dll has been added to git install directory'
+} catch {
+  throw $_.Exception
+}
